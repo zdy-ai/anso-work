e8708184567772df5163f0e6b3e1253a3345b3b0
zhangdaoyin <zhangdaoyin@ansosz.com>
2022-12-14 18:21:35 +0800

    增加activity学习

---
+++ anso-10-spring-activity/src/main/java/com/activity/anso10springactivity/Anso10SpringActivityApplication.java
@@ -0,0 +1,13 @@
+package com.activity.anso10springactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anso10SpringActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Anso10SpringActivityApplication.class, args);
+    }
+
+}
---
+++ anso-10-spring-activity/src/test/java/com/activity/anso10springactivity/Anso10SpringActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity.anso10springactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anso10SpringActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
